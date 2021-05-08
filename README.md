34a3e6a597b05f5d04a2b8f0841588669c697465
nick fisher <fisher.nick14@gmail.com>
2021-05-08 14:36:18 -0700

    ignoring exceptions in the circuit breaker

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # java-failure-and-resilience
 Sampling testing failure scenarios and dealing with them programmatically
 
-Accompanying Articles
+## Accompanying Articles
 
+### Preventing Cascading Failures
+
+- [Configuring, Testing, and Using Circuit Breakers on Rest API calls with Resilience4j](https://nickolasfisher.com/blog/Configuring-Testing-and-Using-Circuit-Breakers-on-Rest-API-calls-with-Resilience4j)
+- [How to Prevent Certain Exceptions from Tripping a Resilience4j Circuit](https://nickolasfisher.com/blog/How-to-Prevent-Certain-Exceptions-from-Tripping-a-Resilience4j-Circuit)
+
+### General Latency
 - [How to Test Latency with a Mock Server in Java](https://nickolasfisher.com/blog/How-to-Test-Latency-with-a-Mock-Server-in-Java)
 - [How to Retry API Requests in Java using Resilience4j](https://nickolasfisher.com/blog/How-to-Retry-API-Requests-in-Java-using-Resilience4j)
-- [Configuring, Testing, and Using Circuit Breakers on Rest API calls with Resilience4j](https://nickolasfisher.com/blog/Configuring-Testing-and-Using-Circuit-Breakers-on-Rest-API-calls-with-Resilience4j)
+
 
--- synchronous/src/test/java/com/nickolasfisher/resilience/CircuitBreakerTest.java
+++ synchronous/src/test/java/com/nickolasfisher/resilience/CircuitBreakerTest.java
@@ -5,7 +5,6 @@ import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.retry.Retry;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,16 +12,12 @@ import org.mockserver.integration.ClientAndServer;
 import org.mockserver.junit.jupiter.MockServerExtension;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
-import org.mockserver.model.MediaType;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -87,6 +82,96 @@ public class CircuitBreakerTest {
             assertEquals("call-endpoint", callNotPermittedException.getCausingCircuitBreakerName());
             assertSame(CircuitBreaker.State.OPEN, callingEndpointCircuitBreaker.getState());
         }
+    }
 
+    @Test
+    public void clientErrorException_stillTripsTheCircuit() {
+        HttpRequest expectedFirstRequest = HttpRequest.request()
+                .withMethod(HttpMethod.GET.name())
+                .withPath("/some/endpoint/10");
+
+        this.clientAndServer
+                .when(expectedFirstRequest)
+                .respond(HttpResponse.response().withStatusCode(404));
+
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig
+                .custom()
+                .slidingWindowSize(10)
+                .build();
+
+        CircuitBreakerRegistry circuitBreakerRegistry =
+                CircuitBreakerRegistry.of(circuitBreakerConfig);
+
+        CircuitBreaker callingEndpointCircuitBreaker = circuitBreakerRegistry.circuitBreaker("call-endpoint");
+
+        // force the circuit to trip
+        for (int i = 1; i < 11; i++) {
+            try {
+                callingEndpointCircuitBreaker.decorateSupplier(() ->
+                        restTemplate.getForEntity("/some/endpoint/10", JsonNode.class)
+                ).get();
+                fail("we should never get here!");
+            } catch (HttpClientErrorException e) {
+                // expected
+            }
+        }
+
+        // circuit is now tripped, but should it be?
+        try {
+            callingEndpointCircuitBreaker.decorateSupplier(() ->
+                    restTemplate.getForEntity("/some/endpoint/10", JsonNode.class)
+            ).get();
+            fail("we should never get here!");
+        } catch (CallNotPermittedException callNotPermittedException)  {
+            assertEquals("call-endpoint", callNotPermittedException.getCausingCircuitBreakerName());
+            assertSame(CircuitBreaker.State.OPEN, callingEndpointCircuitBreaker.getState());
+        }
+    }
+
+    @Test
+    public void excludingClientErrorExceptions_fromTheCount() {
+        HttpRequest expectedFirstRequest = HttpRequest.request()
+                .withMethod(HttpMethod.GET.name())
+                .withPath("/some/endpoint/10");
+
+        this.clientAndServer
+                .when(expectedFirstRequest)
+                .respond(HttpResponse.response().withStatusCode(404));
+
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig
+                .custom()
+                .ignoreException(throwable -> {
+                    return throwable instanceof HttpClientErrorException;
+                })
+                .slidingWindowSize(10)
+                .build();
+
+        CircuitBreakerRegistry circuitBreakerRegistry =
+                CircuitBreakerRegistry.of(circuitBreakerConfig);
+
+        CircuitBreaker callingEndpointCircuitBreaker = circuitBreakerRegistry.circuitBreaker("call-endpoint");
+
+        // before we ignored the exception above, this would trip the circuit
+        for (int i = 1; i < 11; i++) {
+            try {
+                callingEndpointCircuitBreaker.decorateSupplier(() ->
+                        restTemplate.getForEntity("/some/endpoint/10", JsonNode.class)
+                ).get();
+                fail("we should never get here!");
+            } catch (HttpClientErrorException e) {
+                // expected
+            }
+        }
+
+        // the circuit doesn't trip
+        try {
+            callingEndpointCircuitBreaker.decorateSupplier(() ->
+                    restTemplate.getForEntity("/some/endpoint/10", JsonNode.class)
+            ).get();
+            fail("we should never get here!");
+        } catch (HttpClientErrorException httpClientErrorException)  {
+            assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
+            assertSame(CircuitBreaker.State.CLOSED, callingEndpointCircuitBreaker.getState());
+        }
     }
 }
